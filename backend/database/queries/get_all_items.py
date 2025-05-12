cd8e37cb864847d80cc893a0f7c423dcac498efb
fsimonelli <franciscosimonelli6@gmail.com>
2025-05-11 22:39:17 -0300

    fix: openai processing and supabase

--- backend/database/queries/get_all_items.py
+++ backend/database/queries/get_all_items.py
@@ -1,11 +1,11 @@
-from database.supabase_client import supabase
+from supabase_client import supabase
 
 def get_all_items(processed):
     query = supabase.table("items").select("*")
     if processed:
-        query = query.not_.is_("description", "null")
+        query = query.not_.is_("description", "null").neq("description", "")
     else:
-        query = query.is_("description", "null")
+        query = query.eq("description", '')
     response = query.limit(100).execute()
     return response.data
   
