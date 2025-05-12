65dbe458d428d9d05e51e4f32711af25d3514f7e
fsimonelli <franciscosimonelli6@gmail.com>
2025-05-12 09:29:34 -0300

    fix: adapted qdrant_db creation to supabase

--- backend/database/queries/get_all_items.py
+++ backend/database/queries/get_all_items.py
@@ -1,11 +1,11 @@
-from supabase_client import supabase
+from database.supabase_client import supabase
 
 def get_all_items(processed):
     query = supabase.table("items").select("*")
     if processed:
         query = query.not_.is_("description", "null").neq("description", "")
     else:
-        query = query.eq("description", '')
-    response = query.limit(100).execute()
+        query = query.eq("description", '').limit(100)
+    response = query.execute()
     return response.data
   
