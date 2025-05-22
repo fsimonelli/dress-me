db62156767bec4c1edf6775570a66fa3b3370c5a
fsimonelli <franciscosimonelli6@gmail.com>
2025-05-22 14:52:19 -0300

    chore: continued setup and components included

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="main-header">
+      <a
+        href="http://localhost:3000"
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <h1>Dress Me</h1>
+      </a>
+    </header>
+  );
+}
