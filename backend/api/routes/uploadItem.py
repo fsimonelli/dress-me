f338b8ca6655d050adca2dd22302456b920e5d57
fsimonelli <franciscosimonelli6@gmail.com>
2025-06-15 16:00:17 -0300

    chore: cleanup unused files and endpoints

--- backend/api/routes/uploadItem.py
+++ backend/api/routes/uploadItem.py
@@ -27,36 +27,3 @@ async def upload_item(file: UploadFile):
     res = get_complementing_items(outfit_id, item_idx)
     return [res, suggestions]
 
-@router.post("/openai")
-async def upload_item(file: UploadFile):
-    readFile = await file.read()
-    base64_image = base64.b64encode(readFile).decode("utf-8")
-
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "Describe the item shown in the image, minimizing the use of stopwords. Do it in a single line, with no special characters. Describe its color, pattern, material, type, and any other relevant details.",
-                    },
-                    {
-                        "type": "image_url",
-                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
-                    },
-                ],
-            }
-        ],
-    )
-    description = response.choices[0].message.content
-    embedding = list(text_embedding_model.embed([description]))
-    suggestions = qdrant_query.get_items_by_text_embedding(embedding)
-    [outfit_id, item_idx] = [
-        suggestions.points[0].payload["outfit_id"],
-        suggestions.points[0].payload["item_idx"],
-    ]
-
-    res = get_complementing_items(outfit_id, item_idx)
-    return [res, suggestions]
